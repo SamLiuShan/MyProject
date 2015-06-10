a1d67c4c4112aae2ecfb46c3c6d57133aa80f6b3
liushanmeng <o2ounion@o2ouniondeMacBook-Pro.local>
2015-06-10 18:01:27 +0800

    debug

---
+++ Demo001/Demo001/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Demo001
+//
+//  Created by o2ounion on 15/6/10.
+//  Copyright (c) 2015年 o2ounion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.orangeColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
